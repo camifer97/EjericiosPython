b270772eab27b69b02cad547b1407c31763b3ed3
Camila <fernandezcami79@gmail.com>
2023-07-28 20:53:28 -0300

    initial commit

---
+++ MadLib.py
@@ -0,0 +1,10 @@
+# user input
+name = input("Ingresa un nombre: ")
+number = input("Ingresa un numero: ")
+city = input("Ingresa el nombre de una ciudad: ")
+hobby = input("Dime tu cosa favorita: ")
+
+madLib = f"Hola! Mi nombre es {name}, tengo {number} años, vivo en {city} y me gusta mucho {hobby}"
+
+print(madLib)
+
