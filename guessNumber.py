a75d618941a7170d48a3f3aace325ad1330c7824
Camila <fernandezcami79@gmail.com>
2023-07-28 21:23:11 -0300

    Guess Number

---
+++ guessNumber.py
@@ -0,0 +1,15 @@
+import random
+
+n = random.randrange(1,100)
+number = int(input("Ingrese un numero del 1 al 100: "))
+
+while n!=number:
+    if n < number:
+        print("El numero ingresado es demasiado grande.")
+        number = int(input("Intentalo nuevamente: "))
+    elif n > number:
+        print("El numero ingresado es demasiado pequeño.")
+        number = int(input("Intentalo nuevamente: "))
+    else:
+        break
+print("Felicitaciones! Has ganado el juego.")
